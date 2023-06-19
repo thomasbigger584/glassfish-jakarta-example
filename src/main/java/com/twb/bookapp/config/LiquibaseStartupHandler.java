8b59778a9435ed54fe38e9ef4f22f6bfb1f19c21
Bigger, Thomas <thomasbigger584@gmail.com>
2023-06-19 09:00:43 +0100

    small refactor with LiquibaseStartupHandler

--- src/main/java/com/twb/bookapp/config/LiquibaseStartupHandler.java
+++ src/main/java/com/twb/bookapp/config/LiquibaseStartupHandler.java
@@ -1,5 +1,7 @@
 package com.twb.bookapp.config;
 
+import jakarta.enterprise.inject.Default;
+import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -16,36 +18,38 @@ import liquibase.exception.CommandExecutionException;
 import liquibase.exception.DatabaseException;
 import org.hibernate.Session;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
 @WebListener
 public class LiquibaseStartupHandler implements ServletContextListener {
-    public static final String PERSISTENCE_UNIT_NAME = "default";
     private static final String CHANGELOG_FILE_LOCATION = "db/changelog.xml";
     private final Logger logger = Logger.getLogger(LiquibaseStartupHandler.class.getSimpleName());
+
+    @Inject
+    @Default
+    private EntityManager em;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         logger.info("Starting liquibase update...");
 
-        try (EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-             EntityManager em = entityManagerFactory.createEntityManager()) {
+        Session session = em.unwrap(Session.class);
+        session.doWork(this::executeLiquibaseUpdate);
+    }
 
-            Session session = em.unwrap(Session.class);
-            session.doWork(connection -> {
-                try {
-                    Database database = DatabaseFactory.getInstance()
-                            .findCorrectDatabaseImplementation(new JdbcConnection(connection));
-                    CommandScope command = new CommandScope(UpdateCommandStep.COMMAND_NAME);
-                    command.addArgumentValue(DbUrlConnectionCommandStep.DATABASE_ARG, database);
-                    command.addArgumentValue(UpdateCommandStep.CHANGELOG_FILE_ARG, CHANGELOG_FILE_LOCATION);
-                    command.execute();
-                    logger.info("Liquibase update completed successfully.");
-                } catch (DatabaseException | CommandExecutionException e) {
-                    throw new SQLException("Failed execute liquibase update", e);
-                }
-            });
+    private void executeLiquibaseUpdate(Connection connection) throws SQLException {
+        try {
+            Database database = DatabaseFactory.getInstance()
+                    .findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            CommandScope command = new CommandScope(UpdateCommandStep.COMMAND_NAME);
+            command.addArgumentValue(DbUrlConnectionCommandStep.DATABASE_ARG, database);
+            command.addArgumentValue(UpdateCommandStep.CHANGELOG_FILE_ARG, CHANGELOG_FILE_LOCATION);
+            command.execute();
+            logger.info("Liquibase update completed successfully.");
+        } catch (DatabaseException | CommandExecutionException e) {
+            throw new SQLException("Failed execute liquibase update", e);
         }
     }
 }
